aabbe8a967be57fde1a064938d7294c8b58b687c
Clayton Riegger <jonfieldthe2nd@gmail.com>
2016-01-24 18:02:18 -0500

    Simplify my stick control method
    I changed the math in the method to use linear algebra instead of
    pythagoras theorem.

--- RobotTestSetup/src/org/usfirst/frc/team1076/robot/ClaytonJoystick.java
+++ RobotTestSetup/src/org/usfirst/frc/team1076/robot/ClaytonJoystick.java
@@ -8,16 +8,18 @@ public class ClaytonJoystick implements IDrivetrainJoystick {
 	public MotorOutput motionForGamepadInput(IGamepad gamepad) {
 		double rawX = gamepad.getAxis(GamepadAxis.LeftX);
 		double rawY = gamepad.getAxis(GamepadAxis.LeftY);
-        double newX = Math.sqrt(Math.pow((rawX - rawY) / 2 - rawX, 2) + 
-                Math.pow((rawY - rawX) / 2 - rawY, 2));
-        double newY = Math.sqrt(Math.pow((rawX + rawY) / 2 - rawX, 2) + 
-                Math.pow((rawY + rawX) / 2 - rawY, 2));
-        if(rawY < -rawX) {
-            newX = -newX;
-        }
-        if(rawY > rawX) {
-            newY = -newY;
-        }
-		return new MotorOutput(newX, newY);
+		if(rawX == 0 && rawY == 0) {
+			return new MotorOutput(0, 0);
+		}
+        double newX = (rawX + rawY) / 2;
+        double newY = (rawX - rawY) / 2;
+        double powerRatio = magnitude(rawX, rawY) / Math.sqrt(newX*newX + newY*newY);
+		return new MotorOutput(newX * powerRatio, newY * powerRatio);
 	}	
+	
+	private double magnitude(double rawX, double rawY) {
+    	double x = rawX * Math.sqrt(1 - rawY*rawY/2);
+    	double y = rawY * Math.sqrt(1 - rawX*rawX/2);
+		return Math.sqrt(x*x + y*y);
+	}
 }
